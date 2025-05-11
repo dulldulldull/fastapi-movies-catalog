9bc277fa3f2c254f8d9ed0add1cabfbeffe96295
dulldulldull <32587479+dulldulldull@users.noreply.github.com>
2025-05-10 21:19:15 -0700

    added post router create_movie with randomized movie_id generation

--- movies-catalog/api/api_v1/movies/views.py
+++ movies-catalog/api/api_v1/movies/views.py
@@ -1,11 +1,18 @@
 from typing import Annotated
 
-from fastapi import Depends, APIRouter
+from fastapi import (
+    Depends,
+    APIRouter,
+    status,
+    Form,
+)
 
 from .dependencies import (
     prefetch_movie,
 )
 from .crud import MOVIES
+
+import random
 
 from schemas.movie import Movie
 
@@ -21,6 +28,25 @@ router = APIRouter(
 )
 def read_all_movies():
     return MOVIES
+
+
+@router.post(
+    "/",
+    response_model=Movie,
+    status_code=status.HTTP_201_CREATED,
+)
+def create_movie(
+    title: Annotated[str, Form()],
+    description: Annotated[str, Form()],
+    genre: Annotated[str, Form()],
+):
+    movie_id = random.randint(0, 1000)
+    return Movie(
+        movie_id=movie_id,
+        title=title,
+        description=description,
+        genre=genre,
+    )
 
 
 @router.get(
