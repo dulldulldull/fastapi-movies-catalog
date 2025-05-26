eee69eada709f8cdb7c8a4d8c0ecaea6594d1fdc
dulldulldull <32587479+dulldulldull@users.noreply.github.com>
2025-05-25 20:52:59 -0700

    refactored views. Added shared "response"for get and delete movies views

--- movies-catalog/api/api_v1/movies/dependencies.py
+++ movies-catalog/api/api_v1/movies/dependencies.py
@@ -13,5 +13,5 @@ def prefetch_movie(
         return movie
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
-        detail=f"Movie slug {slug!r} not found",
+        detail=f"Movie {slug!r} not found",
     )
---
+++ movies-catalog/api/api_v1/movies/views/list_views.py
@@ -0,0 +1,36 @@
+from fastapi import (
+    APIRouter,
+    status,
+)
+
+from api.api_v1.movies.crud import storage
+
+
+from schemas.movie import (
+    Movie,
+    MovieCreate,
+)
+
+router = APIRouter(
+    prefix="/movies",
+    tags=["Movies"],
+)
+
+
+@router.get(
+    "/",
+    response_model=list[Movie],
+)
+def read_all_movies():
+    return storage.get()
+
+
+@router.post(
+    "/",
+    response_model=Movie,
+    status_code=status.HTTP_201_CREATED,
+)
+def create_movie(
+    movie_create: MovieCreate,
+) -> Movie:
+    return storage.create(movie_create)
